e6fd212c97ee230357696f4bdd1a37d85c97d00c
kyle8615 <kyle8615@gmail.com>
2022-08-16 18:09:46 +0800

    fix: correct-git-command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 git_setup() {
   cat <<- EOF > $HOME/.netrc
-		machine github.com
-		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
-		machine api.github.com
-		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
+    machine github.com
+    login $GITHUB_ACTOR
+    password $GITHUB_TOKEN
+    machine api.github.com
+    login $GITHUB_ACTOR
+    password $GITHUB_TOKEN
 EOF
   chmod 600 $HOME/.netrc
 
@@ -25,19 +25,15 @@ git_cmd() {
 }
 
 PR_BRANCH="auto-$INPUT_PR_BRANCH-$GITHUB_SHA"
-MESSAGE=$(git log -1 $GITHUB_SHA | grep "AUTO" | wc -l)
 
-if [[ $MESSAGE -gt 0 ]]; then
-  echo "Autocommit, NO ACTION"
-  exit 0
-fi
-
-PR_TITLE=$(git log -1 --format="%s" $GITHUB_SHA)
 
 git_setup
+
+PR_TITLE=$(git log -1 --pretty=format:"%s")
+
 git_cmd git remote update
 git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
 git_cmd git cherry-pick "${GITHUB_SHA}"
 git_cmd git push -u origin "${PR_BRANCH}"
-git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
+git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"(AUTO-CHERRY-PICK) ${PR_TITLE}\""
